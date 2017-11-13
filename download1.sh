e9b4f56d715ee70928cc95354820d6934af16222
john <chen.john@nuwarobotics.com>
2017-11-13 14:28:23 +0800

    Mon Nov 13 14:28:23 CST 2017

--- download1.sh
+++ download1.sh
@@ -1,1 +1,3 @@
-wget "http://192.168.1.232:8081/nexus/service/local/artifact/maven/redirect?r=releases&g=com.nuwarobotics.IOTManager&a=IOTManager-release&v=LATEST&e=apk" --content-disposition
+wget --trust-server-names "http://192.168.1.232:8081/nexus/service/local/artifact/maven/redirect?r=releases&g=com.nuwarobotics.KiwiService&a=KiwiService_signed&v=LATEST&e=apk" 
+
+wget --trust-server-names "http://192.168.1.232:8081/nexus/service/local/artifact/maven/redirect?r=releases&g=com.nuwarobotics.KiwiService&a=KiwiService_signed&v=LATEST&e=apk" 
